d9521dced7060c7651594ef465b920648693189c
Your Name <you@example.com>
2025-05-22 21:09:07 +0800

    修改权限

--- singleTest/lidar_filter_pkg.sh
+++ singleTest/lidar_filter_pkg.sh
@@ -1,2 +1,2 @@
 source /home/HwHiAiUser/ros/install/setup.sh
-ros2 run lidar_filter_pkg lidar_filter_node
+ros2 run lidar_filter_pkg lidar_filter
--- singleTest/urdf.sh
+++
@@ -1,2 +0,0 @@
-source /home/HwHiAiUser/ros/install/setup.sh
-ros2 launch sam_bot_description display.launch.py
