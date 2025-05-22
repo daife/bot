c195744b8cd9f10a850c09ed953e0d2d6985e297
Your Name <you@example.com>
2025-05-22 21:15:45 +0800

    删掉原来雷达厂商不需要的构建缓存文件

--- Nodes/oradar_lidar/build/oradar_lidar/sdk/core/math/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/HwHiAiUser/oradar_ros/sdk/core/math
-# Build directory: /home/HwHiAiUser/oradar_ros/build/oradar_lidar/sdk/core/math
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Nodes/oradar_lidar/build/oradar_lidar/sdk/samples/CMakeFiles/non-blocking_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for non-blocking_test.
-# This may be replaced when dependencies are built.
